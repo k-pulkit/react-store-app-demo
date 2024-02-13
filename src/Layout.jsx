f83da579d56342b80015ac9e6bf4d5d29d956482
k-pulkit <pulkit42041@gmail.com>
2024-02-13 18:22:32 -0500

    Fix footer with padding

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,14 +7,14 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <main id="main" className="relative w-full bg-gray-200 min-h-screen">
+    <main id="main" className="relative w-full bg-gray-200 min-h-screen pb-28">
         <div className="sticky top-0 z-50 max-container flex justify-center">
           <Nav />
         </div>
 
         <Outlet />
         
-        <div className='absolute bottom-0 w-full mt-10'>
+        <div className='absolute bottom-0 mt-20 w-full'>
           <Footer />
         </div>
     </main>
