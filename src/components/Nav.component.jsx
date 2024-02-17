be410cde075e401aa4d4025d89c19d810ffad3a7
k-pulkit <pulkit42041@gmail.com>
2024-02-17 00:00:00 +0000

    Nav drop down menu

--- src/components/Nav.component.jsx
+++ src/components/Nav.component.jsx
@@ -10,6 +10,7 @@ const Nav = () => {
   const activeClass = "bg-white text-slate-900 rounded-xl font-bold outline outline-green-700"
   const inactiveClass = "hover:underline outline outline-transparent"
   const navClass = ({isActive}) => (`px-2 py-1 font-semibold text-center self-center hover:cursor-pointer ${isActive ? activeClass : inactiveClass}`)
+  const navClass_hiddenMenu = `w-full pl-10 text-start ${navClass}`
   return (
     <div className={`text-xl mt-3 px-[4%] py-3 w-[95%] shadow-zinc-600 shadow-lg rounded-lg leading-normal flex flex-row justify-between items-center font-palanquin text-white transition ease-in duration-300 
                      ${scrolledDown ? "bg-blue-900": "bg-blue-700 bg-opacity-95"}`}>
@@ -27,8 +28,14 @@ const Nav = () => {
             </NavLink>
         </div>
 
-        <div className="flex flex-row gap-3 md:hidden">
+        <div className="relative flex flex-row gap-3 md:hidden">
           <RxHamburgerMenu />
+          <div className='flex flex-col w-[10rem] justify-center items-center max-md:mx-2 mx-6 max-sm:my-9 my-6 py-3 border-2 border-blue-950 absolute -top-0 right-0 text-white bg-blue-800 rounded-lg'>
+            <NavLink to="/shop" end className={navClass_hiddenMenu}>Shop</NavLink>
+            <NavLink to="/contact" end className={navClass_hiddenMenu}>Contact</NavLink>
+            <NavLink to="/signin" end className={navClass_hiddenMenu}>Signin</NavLink>
+            <NavLink to="/cart" end className={navClass_hiddenMenu}>Cart</NavLink>
+          </div>
         </div>
     </div>
   )
