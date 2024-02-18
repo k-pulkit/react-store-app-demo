115a27b9e213e7002254eb3beeebe365177b214f
k-pulkit <pulkit42041@gmail.com>
2024-02-18 00:00:00 +0000

    Config to deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-store-app-demo",
   plugins: [react()],
+  server: {
+    host: true
+  }
 })
